3bf622fc6f51bf8aefe63cbf3a2836197569ae70
elliotmassen <elliotm@greenshootlabs.com>
2019-07-30 14:31:38 +0100

    Update ContextService dependency injection to use Facade

--- app/Bot/Actions/RefreshUserAction.php
+++ app/Bot/Actions/RefreshUserAction.php
@@ -5,7 +5,7 @@ namespace App\Bot\Actions;
 use OpenDialogAi\ActionEngine\Actions\ActionInput;
 use OpenDialogAi\ActionEngine\Actions\ActionResult;
 use OpenDialogAi\ActionEngine\Actions\BaseAction;
-use OpenDialogAi\ContextEngine\ContextManager\ContextService;
+use OpenDialogAi\ContextEngine\Facades\ContextService;
 
 class RefreshUserAction extends BaseAction
 {
@@ -20,10 +20,7 @@ class RefreshUserAction extends BaseAction
 
     public function perform(ActionInput $actionInput): ActionResult
     {
-        /** @var ContextService $contextService */
-        $contextService = app()->make(ContextService::class);
-
-        $userContext = $contextService->getUserContext();
+        $userContext = ContextService::getUserContext();
 
         collect(self::$userAttributes)->each(function (string $attributeName) use ($userContext) {
             $userContext->removeAttribute($attributeName);
